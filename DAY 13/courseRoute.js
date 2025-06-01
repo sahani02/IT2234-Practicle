c48e990694e65c8d1578ae8d21a258d59fffc46a
sahani02 <2002sahani@gmail.com>
2025-06-01 07:55:12 -0700

    Add files via upload

---
+++ DAY 13/courseRoute.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const Course = require('../Models/Course')
+
+router.get('/', async (req,res)=>{
+    try {
+        const results = await Course.find()
+        if(results){
+            res.status(200).json(results)
+        }else{
+            res.status(404).send("Sorry, No data Found !")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error !")
+    }
+})
+
+router.get('/:id', async (req,res)=>{
+    try {
+        const id = req.params.id
+        const results = await Course.findById(id)
+        if(results){
+            res.status(200).json(results)
+        }else{
+            res.status(404).send("Sorry, No data Found !")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error !")
+    }
+})
+
+router.get('/code/:cid', async (req,res)=>{
+    try {
+        const cid = req.params.cid
+        const results = (await Course.find({code:cid}))
+        const count = results.length
+        console.log(count)
+        if (results) {
+            if (count>0){
+                res.status(200).json(results)
+            }
+            else {
+                res.status(404).send("Sorry, No Data Found !")
+            }  
+           
+        } else {
+            res.status(404).send("Sorry, No Data Found !")
+        }  
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error !")
+    }   
+})
+
+module.exports=router
